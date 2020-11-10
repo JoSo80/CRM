2bba376b1da90656d83fa3603ea0fd0055c93bf4
JoSo80 <57719543+JoSo80@users.noreply.github.com>
2020-11-09 22:48:43 -0600

    Add files via upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_012344) do
+ActiveRecord::Schema.define(version: 2020_11_10_033043) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 2020_11_10_012344) do
   create_table "customers", force: :cascade do |t|
     t.string "full_name"
     t.string "phone_number"
-    t.string "email_address"
+    t.string "email"
     t.text "notes"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
